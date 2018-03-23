a87ea0d2c8b7f65209509b3c070994e7ab21d600
adikalova <adikalova@gmail.com>
2018-03-23 21:08:37 +0200

    added todoist.apk added AndroidSetup for ToDoist added desired_capabilities_appium.txt

---
+++ src/test/java/todoist/AndroidSetupToDoist.java
@@ -0,0 +1,26 @@
+package todoist;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AndroidSetupToDoist {
+    protected AndroidDriver driver;
+
+    protected void prepareAndroidForAppium() throws MalformedURLException {
+        File appDir = new File("app");
+        File app = new File(appDir, "com.todoist-12.7@APK4Fun.com.apk");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("device", "Android");
+        capabilities.setCapability("deviceName", "emulator-5554");
+        capabilities.setCapability("appActivity", "com.todoist.activity.HomeActivity");
+        capabilities.setCapability("appPackage", "com.todoist");
+        capabilities.setCapability("app", app.getAbsolutePath());
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+}
