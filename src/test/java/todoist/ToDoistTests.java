3869d6360e03645d5cdaab96239d9ea09318adeb
adikalova <adikalova@gmail.com>
2018-03-23 22:09:16 +0200

    added LoginTest

--- src/test/java/todoist/ToDoistTests.java
+++ src/test/java/todoist/ToDoistTests.java
@@ -9,6 +9,13 @@ import org.testng.annotations.Test;
 public class ToDoistTests  extends AndroidSetupToDoist {
     String app_package_name = "com.todoist:id/";
     By continueLogin = By.id(app_package_name + "btn_welcome_continue_with_email");
+    By otherButton = By.id("com.google.android.gms:id/cancel");
+    By emailInput = By.id(app_package_name + "email_exists_input");
+    By continueWithEmailButton = By.id(app_package_name + "btn_continue_with_email");
+    By passwordInput = By.id(app_package_name + "log_in_password");
+    By loginButton = By.id(app_package_name +  "btn_log_in");
+    By toDoisticon = By.id(app_package_name + "id/empty_title");
+
 
 
 
@@ -23,10 +30,16 @@ public class ToDoistTests  extends AndroidSetupToDoist {
     }
 
     @Test
-    public void showTest() {
+    public void LoginTest() throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(driver, 30);
         driver.findElement(continueLogin).click();
 
+        Thread.sleep(5000);
 
-}
+        driver.findElement(otherButton).click();
+        driver.findElement(emailInput).sendKeys("hillelqaauto52@gmail.com");
+        driver.findElement(continueWithEmailButton).click();
+        driver.findElement(passwordInput).sendKeys("welcome2hillel");
+        driver.findElement(loginButton).click();
+    }
 }
