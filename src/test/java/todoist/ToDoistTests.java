d36ab7e3329f13bdb4564d0d4156bc5eb283756d
Dima Spektor <d.spektor.v@gmail.com>
2018-03-29 02:46:42 +0300

    finished search test. todo implement task deletion and waits

--- src/test/java/todoist/ToDoistTests.java
+++ src/test/java/todoist/ToDoistTests.java
@@ -1,10 +1,13 @@
 package todoist;
 
+import io.appium.java_client.MobileElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 import java.util.List;
 
@@ -21,6 +24,9 @@ public class ToDoistTests  extends AndroidSetupToDoist {
     By searchEditText = By.id(app_package_name + "search_edit_text");
     By foundItemsLocator = By.xpath("//*[@resource-id='com.todoist:id/global_search_results']" +
             "/descendant::android.widget.TextView[@resource-id='com.todoist:id/text']");
+    By searchTabTASKS = By.xpath("//*[@resource-id='com.todoist:id/global_search_tabs']//*[@text='TASKS']");
+    By addButton = By.id(app_package_name + "fab");
+    By backButton = By.id(app_package_name + "action_mode_close_button");
 
 
 
@@ -42,24 +48,50 @@ public class ToDoistTests  extends AndroidSetupToDoist {
         Thread.sleep(5000);
 
         driver.findElement(otherButton).click();
-        driver.findElement(emailInput).sendKeys("hillelqaauto52@gmail.com");
+        driver.findElement(emailInput).sendKeys("hillelqaauto5@gmail.com");
         driver.findElement(continueWithEmailButton).click();
-        driver.findElement(passwordInput).sendKeys("welcome2hillel");
+        driver.findElement(passwordInput).sendKeys("123123");
         driver.findElement(loginButton).click();
 //TODO replace sleep with wait.until
         Thread.sleep(5000);
-        driver.findElement(emptyIcon).isDisplayed();
+//        driver.findElement(emptyIcon).isDisplayed(); //test fails if there are any tasks created
+        driver.findElement(addButton).isDisplayed();
     }
 
-    @Test(dependsOnMethods = "LoginTest()")
-    public void SearchTest() throws InterruptedException {
-        driver.findElement(searchButton).click();
+    @Test(dependsOnMethods = {"LoginTest"})
+    public void searchTest() throws InterruptedException {
         String searchString = "hello";
-        driver.findElement(searchEditText).sendKeys(searchString);
-        List foundItems = driver.findElements(foundItemsLocator);
+        int counter = 0;
+        try { //TODO replace with wait until is displayed or move on by timeout
+            driver.findElement(emptyIcon);
+            addTasksBulk("HelloWorld", "Hello", "hello", "hell", "byehellobye", "notMatch", "HELLOAGAIN"); // 5 matches
+        } catch (NoSuchElementException e){}
 
-        //todo for(item in items)   assert
+        driver.findElement(searchButton).click();
+        driver.findElement(searchEditText).sendKeys(searchString, "\n");
+        driver.findElement(searchTabTASKS).click();
+        Thread.sleep(5000); //TODO replace with wait until roller disappears
+        List<MobileElement> foundItems = driver.findElements(foundItemsLocator);
 
+        for (MobileElement item : foundItems) {
+            assertTrue(item.getAttribute("text").toLowerCase().contains(searchString));
+            counter++;
+        }
+        System.out.println("Found results counter = " + counter);
+        deleteAllTasks();
+    }
 
+    public void addTasksBulk(String... taskList) {
+        if (0 == taskList.length) return;
+        driver.findElement(addButton).click();
+        for (String task : taskList) {
+            driver.findElementById("android:id/message").sendKeys(task, "\n");
+            System.out.println("New task '" + task + "' is created");
+        }
+        driver.findElement(backButton).click();
+        System.out.println(taskList.length + " tasks have been created");
+    }
+    public void deleteAllTasks(){
+//        TODO implement deletion of all tasks
     }
 }
